2a200303c5dc6482dc8ce78b32bc11c4201ca9c7
firstad1766 <57754402+firstad1766@users.noreply.github.com>
2020-05-17 16:05:59 +0500

    анимировала слайдеры

--- technomart/js/script.js
+++ technomart/js/script.js
@@ -1,3 +1,56 @@
+/* slider */
+var slides = document.querySelectorAll(".slide");
+var switchers = document.querySelectorAll(".switcher");
+var arrowNext = document.querySelector(".next");
+var arrowPrevious = document.querySelector(".previous");
+var slideInterval = setInterval(nextSlide, 2000);
+var current = 0;
+
+arrowNext.addEventListener("click", function(){
+  nextSlide();
+});
+
+arrowPrevious.addEventListener("click", function(){
+  slides[current].classList.remove("slide-current");
+  switchers[current].classList.remove("switcher-current");
+  if (current === 0) {
+    current = slides.length;
+  }
+  current = (current - 1);
+  slides[current].classList.add("slide-current");
+  switchers[current].classList.add("switcher-current");
+});
+
+function nextSlide() {
+  slides[current].classList.remove("slide-current");
+  switchers[current].classList.remove("switcher-current");
+  current = (current + 1)%slides.length;
+  slides[current].classList.add("slide-current");
+  switchers[current].classList.add("switcher-current");
+}
+
+/* slider-services*/
+var serviceLinks = document.querySelectorAll(".service-link");
+var currentLink = 0;
+var serviceInterval = setInterval(nextService, 2000);
+
+function nextService(){
+  serviceLinks[currentLink].classList.remove("service-current");
+  currentLink = (currentLink + 1)%serviceLinks.length;
+  serviceLinks[currentLink].classList.add("service-current");
+}
+
+for (var i = 0; i < serviceLinks.length; i++) {
+  serviceLinks[i].addEventListener("click", function(evt){
+    evt.preventDefault();
+    var serviceCurrent = document.querySelector(".service-current");
+    serviceCurrent.classList.remove("service-current");
+    this.classList.add("service-current");
+    currentLink = this.getAttribute("name");
+  });
+  serviceLinks[i].setAttribute("name", i);
+}
+
 /* modal map */
 var mapLink = document.querySelector(".show-map");
 var map = document.querySelector(".modal-map");
